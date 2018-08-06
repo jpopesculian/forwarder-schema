8bc76f43cee80a6b40070fabad3000177295e483
Julian Popescu <jpopesculian@gmail.com>
2018-08-06 15:10:41 +0200

    improved texts query with param

--- lib/forwarder_schema/fields/texts_field.rb
+++ lib/forwarder_schema/fields/texts_field.rb
@@ -6,11 +6,12 @@ module ForwarderSchema
       included do
         field :texts, [Types::TextType], null: true do
           description "Get a list of texts"
+          argument :number, String, required: false
         end
       end
 
-      def texts
-        Models::Text.all
+      def texts(number: nil)
+        Models::Queries::TextByContact.(number)
       end
     end
   end
--- lib/forwarder_schema/types/date_time_type.rb
+++ lib/forwarder_schema/types/date_time_type.rb
@@ -4,10 +4,18 @@ module ForwarderSchema
       description "Holds a date and time"
 
       def self.coerce_input(value, _ctx)
-        Time.zone.parse(value)
+        case value
+        when String
+          Time.parse(value)
+        when Date
+          value.to_time
+        else
+          value
+        end
       end
 
       def self.coerce_result(value, _ctx)
+        value = coerce_input(value, _ctx) unless value.is_a? Time
         value.utc.iso8601
       end
     end
